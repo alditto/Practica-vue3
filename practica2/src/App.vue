c90f129043618b32c3b16f06cd04615b7b870cb1
Alditto <aldito_wachin@hotmail.com>
2023-07-12 19:16:28 -0400

    footer

--- practica2/src/App.vue
+++ practica2/src/App.vue
@@ -25,7 +25,28 @@
   
   <router-view/>
 
-  
+  <footer class="page-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Pie de pagina</h5>
+          <p class="grey-text text-lighten-4">Practica 2.</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Links</h5>
+          <ul>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+            
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">© 2023 Copyright
+      </div>
+    </div>
+  </footer> 
 </template>
 
 <style lang="scss">
